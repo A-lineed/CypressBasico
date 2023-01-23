ea980f2413d0cc1d8d93de1a550511e447d98218
A-lineed <aline.edvania.franca@outlook.com>
2023-01-23 10:40:54 -0300

    Acessando uma página e realizando assertivas com seu titulo

---
+++ cypress/e2e/basic.spec.cy.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+
+describe('Cypress basics', () => {
+    it('Should visit a page and assert title', () => {
+        cy.visit('https://www.wcaquino.me/cypress/componentes.html')
+
+
+       // cy.title().should('be.equal', 'Campo de Treinamento')
+        //cy.title().should('contain', 'Campo')
+
+        cy.title()
+            .should('be.equal', 'Campo de Treinamento')
+            .and('contain', 'Campo')
+
+
+
+    })
+})
