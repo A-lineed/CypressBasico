7c421e6be36285772946cafefba0f4818949ffd7
A-lineed <aline.edvania.franca@outlook.com>
2023-01-25 15:24:35 -0300

    Localizando e interagindo com um elemento

--- cypress/e2e/basic.spec.cy.js
+++ cypress/e2e/basic.spec.cy.js
@@ -5,14 +5,21 @@ describe('Cypress basics', () => {
         cy.visit('https://www.wcaquino.me/cypress/componentes.html')
 
 
-       // cy.title().should('be.equal', 'Campo de Treinamento')
+        // cy.title().should('be.equal', 'Campo de Treinamento')
         //cy.title().should('contain', 'Campo')
 
         cy.title()
             .should('be.equal', 'Campo de Treinamento')
             .and('contain', 'Campo')
 
+    })
 
+    it.only('Should find and interact with an element', () => {
+        cy.visit('https://www.wcaquino.me/cypress/componentes.html')
+
+        cy.get('#buttonSimple')
+            .click()
+            .should('have.value', 'Obrigado!');
 
     })
 })
