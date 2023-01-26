a8a73ddd3677f937e642595f583c60d0a86004a8
A-lineed <aline.edvania.franca@outlook.com>
2023-01-26 12:13:47 -0300

    Elemento - campos de texto

--- cypress/e2e/elements.spec.cy.js
+++ cypress/e2e/elements.spec.cy.js
@@ -1,9 +1,11 @@
+/// <reference types="cypress" />
+
 describe('work with basic elements', () => {
     beforeEach(() => {
         cy.visit('https://www.wcaquino.me/cypress/componentes.html')
 
     })
-    
+
     it('Text', () => {
         cy.get('body').should('contain', 'Cuidado')
         cy.get('span').should('contain', 'Cuidado')
@@ -19,5 +21,26 @@ describe('work with basic elements', () => {
         cy.get('#resultado').should('have.not.text', 'Voltou!')
         cy.contains('Voltar').click()
         cy.get('#resultado').should('have.text', 'Voltou!')
+    })
+
+    it.only('TextFields', () => {
+        cy.get('#formNome').type('Teste Cypress')
+        cy.get('#formNome').should('have.value', 'Teste Cypress')
+
+        cy.get('#elementosForm\\:sugestoes')
+            .type('textarea')
+            .should('have.value', 'textarea')
+
+        cy.get('#tabelaUsuarios > :nth-child(2) > :nth-child(1) > :nth-child(6) > input').type('???')
+
+        cy.get('[data-cy="dataSobrenome"]')
+            .type('Teste12345{backspace}{backspace}')
+            .should('have.value', 'Teste123')
+
+        cy.get('#elementosForm\\:sugestoes')
+            .clear()
+            .type('Erro{selectall}acerto', { delay: 100 })
+            .should('have.value', 'acerto')
+
     })
 })
