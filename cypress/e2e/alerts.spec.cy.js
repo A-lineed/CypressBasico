21a507957acee6060a8b0f147256d8a7fea8c1c6
A-lineed <aline.edvania.franca@outlook.com>
2023-02-04 10:57:48 -0300

    Alert com mock

--- cypress/e2e/alerts.spec.cy.js
+++ cypress/e2e/alerts.spec.cy.js
@@ -10,12 +10,21 @@ describe('work with alerts', () => {
 
     })
 
-    it("Alerts", () => {
+    it("Alert", () => {
         cy.get('#alert').click()
         cy.on('window:alert', msg => {
             console.log(msg)
             expect(msg).to.be.equal('Alert Simples')
         })
+    })
+
+    it.only("Alert com mock", () => {
+        const stub = cy.stub().as("Alerta")
+        cy.on('window:alert', stub)
+        cy.get('#alert').click().then(() => {
+            expect(stub.getCall(0)).to.be.calledWith('Alert Simples')
+        })
+
     })
 
 })
