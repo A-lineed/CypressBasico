2fa69f5b473a7721716bce14c0c844ef8b230416
A-lineed <aline.edvania.franca@outlook.com>
2023-02-04 11:37:18 -0300

    Confirm

--- cypress/e2e/alerts.spec.cy.js
+++ cypress/e2e/alerts.spec.cy.js
@@ -18,13 +18,34 @@ describe('work with alerts', () => {
         })
     })
 
-    it.only("Alert com mock", () => {
+    it("Alert com mock", () => {
         const stub = cy.stub().as("Alerta")
         cy.on('window:alert', stub)
         cy.get('#alert').click().then(() => {
             expect(stub.getCall(0)).to.be.calledWith('Alert Simples')
         })
 
+    })
+
+    it.only("Confirm", () => {
+        cy.on('window:confirm', msg => {
+            expect(msg).to.be.equal('Confirm Simples')
+        })
+        cy.on('window:alert', msg => {
+            expect(msg).to.be.equal('Confirmado')
+        })
+        cy.get('#confirm').click()
+    })
+
+    it.only("Deny", () => {
+        cy.on('window:confirm', msg => {
+            expect(msg).to.be.equal('Confirm Simples')
+            return false;
+        })
+        cy.on('window:alert', msg => {
+            expect(msg).to.be.equal('Negado')
+        })
+        cy.get('#confirm').click()
     })
 
 })
