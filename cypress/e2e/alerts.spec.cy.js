b9a9c97ec7b851c026aa3ee7eee7ddb7a065bb1a
A-lineed <aline.edvania.franca@outlook.com>
2023-02-06 12:42:21 -0300

    Prompt

--- cypress/e2e/alerts.spec.cy.js
+++ cypress/e2e/alerts.spec.cy.js
@@ -37,7 +37,7 @@ describe('work with alerts', () => {
         cy.get('#confirm').click()
     })
 
-    it.only("Deny", () => {
+    it("Deny", () => {
         cy.on('window:confirm', msg => {
             expect(msg).to.be.equal('Confirm Simples')
             return false;
@@ -47,5 +47,19 @@ describe('work with alerts', () => {
         })
         cy.get('#confirm').click()
     })
+
+    it.only("Prompt", () => {
+        cy.window().then(win => {
+            cy.stub(win, 'prompt').returns('50')
+        })
+        cy.on('window:confirm', msg => {
+            expect(msg).to.be.equal('Era 50?')
+        })
+        cy.on('window:alert', msg => {
+            expect(msg).to.be.equal(':D')
+        })
+        cy.get('#prompt').click()
+    })
+
 
 })
