ce858409cdd752fe94bb9d615e952e53e32678a9
A-lineed <aline.edvania.franca@outlook.com>
2023-02-08 16:26:53 -0300

    Popup via links

--- cypress/e2e/popup.spec.cy.js
+++ cypress/e2e/popup.spec.cy.js
@@ -21,5 +21,31 @@ describe('work with Popup', () => {
     })
 
 
+    describe.only('With links', () => {
+        beforeEach(() => {
+            cy.visit('https://www.wcaquino.me/cypress/componentes.html')
+        })
+        it('Check popup url', () => {
+            cy.contains('Popup2')
+                .should('have.prop', 'href')
+                .and('equal', 'https://www.wcaquino.me/cypress/frame.html')
+        })
+
+        it('Should access popup dinamically', () => {
+            cy.contains('Popup2').then($a => {
+                const href = $a.prop('href')
+                cy.visit(href)
+                cy.get('#tfield').type('funciona')
+            })
+        })
+
+        it('Should force link on same page ', () => {
+            cy.contains('Popup2')
+                .invoke('removeAttr', 'target')
+                .click()
+            cy.get('#tfield').type('funciona também')
+        })
+
+    })
 
 })
